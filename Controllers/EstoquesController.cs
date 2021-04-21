415f6f9012b51522b8cc01c389a8818df79c3161
Bruno Almeida <bruno.almeida32@aluno.unip.br>
2021-04-20 22:35:30 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/EstoquesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoulMV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoulMV.Controllers
+{
+    public class EstoquesController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Estoque> list = new List<Estoque>();
+            list.Add(new Estoque { Codigo = 1, Nome = "Almox Central" });
+            list.Add(new Estoque { Codigo = 2, Nome = "Almox Medicamento" });
+
+
+            return View(list);
+        }
+    }
+}
---
+++ Models/Estoque.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoulMV.Models
+{
+    public class Estoque
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
